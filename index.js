f3745a9834064089d694f046cae6620eb3e33e19
belitajosephgabriel <belitajosephgabriel@gmail.com>
2025-09-23 04:20:32 +0800

    Fix index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const workoutRoutes = require("./routes/workoutRoutes");
 require('dotenv').config();
 
 const app = express()
-const port = 4000;
 
 mongoose.connect(process.env.MONGODB_STRING);
 mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atlas'));
@@ -26,7 +25,9 @@ app.use("/users", userRoutes);
 app.use("/workouts", workoutRoutes);
 
 if(require.main === module){
-    app.listen(port, () => console.log(`Server running at port ${port}`));
+	app.listen(process.env.PORT || 3000, () => {
+		console.log(`API is now online on port ${ process.env.PORT || 3000}`)
+	});
 }
 
 module.exports = { app, mongoose};
